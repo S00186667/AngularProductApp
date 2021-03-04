eb51d964ec4cfd7b704ad2efdecc14d60bd0a878
carla-mulligan <s00186667@mail.itsligo.ie>
2021-03-04 09:43:31 +0000

    UI Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,16 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { AppRoutingModule } from './app-routing.module';
 import { ProductFormComponent } from './product-form/product-form.component';
 //import {RouterModule } from '@angular/router'; 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'; 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HeaderComponent } from './componets/shared/header/header.component';
+import { FooterComponent } from './componets/shared/footer/footer.component';
+import { NavComponent } from './componets/shared/nav/nav.component';
+import { ShoppingListComponent } from './componets/shopping-list/shopping-list.component';
+import { FiltersComponent } from './componets/shopping-list/filters/filters.component';
+import { CartComponent } from './componets/shopping-list/cart/cart.component';
+import { CartItemComponent } from './componets/shopping-list/cart-item/cart-item.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+
 
 
 @NgModule({
@@ -20,7 +29,16 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     ProductListComponent,
     ProductRowComponent,
     ProductDetailsComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingListComponent,
+    FiltersComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent,
+    
   ],
   imports: [
     BrowserModule, 
